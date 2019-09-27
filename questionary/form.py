e4feba6c8d71fb4f078ca6530b202adb6f6143f3
Shaun Read <shaun.c.read@gmail.com>
2019-09-27 14:31:33 +0200

    fix no arguments

--- questionary/form.py
+++ questionary/form.py
@@ -39,7 +39,7 @@ class Form:
         while not all(f.key in answers for f in self.form_fields):
             for field in self.form_fields:
                 if field.key in answers:
-                    def skip(): return True
+                    def skip(x): return True
                 else:
                     skip = self.skip_conditions.get(field.key, lambda x: False)
                 values = {f.question: answers[f.key] for f in self.form_fields
