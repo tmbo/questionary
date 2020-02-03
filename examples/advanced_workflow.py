fc50dac258a5f40c08c4cc138c18495847d2c41e
Matthias <xmatthias@outlook.com>
2020-02-03 10:26:25 +0100

    Run black on top of new example

--- examples/advanced_workflow.py
+++ examples/advanced_workflow.py
@@ -5,37 +5,32 @@ from questionary import Separator, prompt
 def ask_dictstyle(**kwargs):
     questions = [
         {
-            'type': 'confirm',
-            'name': 'conditional_step',
-            'message': 'Would you like the next question?',
-            'default': True,
+            "type": "confirm",
+            "name": "conditional_step",
+            "message": "Would you like the next question?",
+            "default": True,
         },
         {
-            'type': 'text',
-            'name': 'next_question',
-            'message': 'Name this library?',
+            "type": "text",
+            "name": "next_question",
+            "message": "Name this library?",
             # Validate if the first question was answered with yes or no
-            'when': lambda x: x['conditional_step'],
+            "when": lambda x: x["conditional_step"],
             # Only accept questionary as answer
-            'validate': lambda val: val == 'questionary',
+            "validate": lambda val: val == "questionary",
         },
         {
-            'type': 'select',
-            'name': 'second_question',
-            'message': 'Select item',
-            'choices': [
-                    'item1',
-                    'item2',
-                    Separator(),
-                    'other',
-            ],
+            "type": "select",
+            "name": "second_question",
+            "message": "Select item",
+            "choices": ["item1", "item2", Separator(), "other",],
         },
         {
-            'type': 'text',
+            "type": "text",
             # intentionally overwrites result from previous question
-            'name': 'second_question',
-            'message': 'Insert free text',
-            'when': lambda x: x['second_question'] == 'other',
+            "name": "second_question",
+            "message": "Insert free text",
+            "when": lambda x: x["second_question"] == "other",
         },
     ]
     return prompt(questions)
