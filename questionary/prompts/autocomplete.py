1fe4e4646c706788a0d1a0f4f412f5b18c61f882
Zylvian <33404765+Zylvian@users.noreply.github.com>
2020-07-09 10:23:41 +0200

    Removed check for choices if completer is present
    Had a problem supplying my own Completer, as I had to make a dummy list with a single value to get the check to pass. This commit should let people be able to supply **either** a completer or choices for the default completer.

--- questionary/prompts/autocomplete.py
+++ questionary/prompts/autocomplete.py
@@ -149,8 +149,6 @@ def autocomplete(
         Question: Question instance, ready to be prompted (using `.ask()`).
     """
 
-    if not choices:
-        raise ValueError("No choices is given, you should use Text question.")
 
     merged_style = merge_styles([DEFAULT_STYLE, style])
 
@@ -166,7 +164,12 @@ def autocomplete(
 
     validator = build_validator(validate)
 
+    
     if completer is None:
+        
+        if not choices:
+            raise ValueError("No choices is given, you should use Text question.")
+        
         # use the default completer
         completer = WordCompleter(
             choices,
