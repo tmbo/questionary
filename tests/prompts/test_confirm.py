7277bfe929c580f84ffb746ae50add1ee0ace554
Kian Cross <kian@kiancross.co.uk>
2020-12-19 00:10:04 +0000

    Added tests

--- tests/prompts/test_confirm.py
+++ tests/prompts/test_confirm.py
@@ -22,7 +22,7 @@ def test_confirm_enter_default_no():
 
 def test_confirm_yes():
     message = "Foo message"
-    text = "y" + KeyInputs.ENTER + "\r"
+    text = "y" + "\r"
 
     result, cli = feed_cli_with_input("confirm", message, text)
     assert result is True
@@ -30,7 +30,7 @@ def test_confirm_yes():
 
 def test_confirm_no():
     message = "Foo message"
-    text = "n" + KeyInputs.ENTER + "\r"
+    text = "n" + "\r"
 
     result, cli = feed_cli_with_input("confirm", message, text)
     assert result is False
@@ -38,7 +38,7 @@ def test_confirm_no():
 
 def test_confirm_big_yes():
     message = "Foo message"
-    text = "Y" + KeyInputs.ENTER + "\r"
+    text = "Y" + "\r"
 
     result, cli = feed_cli_with_input("confirm", message, text)
     assert result is True
@@ -46,7 +46,7 @@ def test_confirm_big_yes():
 
 def test_confirm_big_no():
     message = "Foo message"
-    text = "N" + KeyInputs.ENTER + "\r"
+    text = "N" + "\r"
 
     result, cli = feed_cli_with_input("confirm", message, text)
     assert result is False
@@ -66,3 +66,19 @@ def test_confirm_ctr_c():
 
     with pytest.raises(KeyboardInterrupt):
         feed_cli_with_input("confirm", message, text)
+
+
+def test_confirm_not_autoenter_yes():
+    message = "Foo message"
+    text = "n" + "y" + KeyInputs.ENTER + "\r"
+
+    result, cli = feed_cli_with_input("confirm", message, text, auto_enter=False)
+    assert result is True
+
+
+def test_confirm_not_autoenter_no():
+    message = "Foo message"
+    text = "n" + "y" + KeyInputs.ENTER + "\r"
+
+    result, cli = feed_cli_with_input("confirm", message, text, auto_enter=False)
+    assert result is True
