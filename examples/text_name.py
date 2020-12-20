174f474098deb1493dff9199ff3ff1eb981dc591
Tom Bocklisch <tom@rasa.com>
2020-12-20 12:49:47 +0100

    undid accidental change

--- examples/text_name.py
+++ examples/text_name.py
@@ -1,11 +1,5 @@
 import questionary
 
 if __name__ == "__main__":
-    import questionary
-
-    answers = questionary.form(
-        first=questionary.confirm("Would you like the next question?", default=True),
-        second=questionary.select("Select item", choices=["item1", "item2", "item3"])
-    ).ask()
-
-    print(answers)
+    name = questionary.text("What's your first name").ask()
+    print(f"Hey {name} 🦄")
