e423af22e7d34797f8eb11da460bd4ef6a67e32a
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2021-02-11 20:59:54 +0100

    [IMP] Pointer to colored hand
    On High-res display's the arrow pointer was not very visible. By changing to colored pointing finger. Readability is improved. Intuitively one will make a selection.

--- questionary/constants.py
+++ questionary/constants.py
@@ -16,7 +16,7 @@ NO_OR_YES = "(y/N)"
 INSTRUCTION_MULTILINE = "(Finish with 'Alt+Enter' or 'Esc, Enter')\n>"
 
 # Selection token used to indicate the selection cursor in a list
-DEFAULT_SELECTED_POINTER = "»"
+DEFAULT_SELECTED_POINTER = "👉"
 
 # Item prefix to identify selected items in a checkbox list
 INDICATOR_SELECTED = "●"
