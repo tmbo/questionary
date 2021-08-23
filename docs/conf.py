1442dde3adcbb99e218cd8b3c18f14d07babfc4a
Kian Cross <kian@kiancross.co.uk>
2021-08-23 01:36:18 +0100

    Upgrade Sphinx to v4

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,11 @@
-import json
 import os
 import sys
-import traceback
 
 sys.path.insert(0, os.path.abspath("../"))
 from questionary import __version__
 
 project = "Questionary"
-copyright = "2020, Questionary"
+copyright = "2021, Questionary"
 author = "Questionary"
 
 version = __version__
@@ -20,12 +18,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
     "sphinx_autodoc_typehints",
-]
-
-nitpick_ignore = [
-    ("py:class", "questionary.prompts.common.Choice"),
-    ("py:class", "questionary.question.Question"),
-    ("py:class", "questionary.form.Form"),
 ]
 
 autodoc_typehints = "description"
@@ -41,50 +33,10 @@ html_theme_options = {
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "prompt_toolkit": ("https://python-prompt-toolkit.readthedocs.io/en/master/", None),
+    "prompt_toolkit": (
+        "https://prompt-toolkit-test.readthedocs.io/en/upgrade-sphinx",
+        None,
+    ),
 }
 
 autodoc_member_order = "alphabetical"
-
-# TODO: remove as soon as we upgrade to sphinx 4.0
-# Borrowed from https://github.com/sphinx-doc/sphinx/issues/5603
-intersphinx_aliases = {
-    ("py:class", "prompt_toolkit.styles.style.Style"): (
-        "py:class",
-        "prompt_toolkit.styles.Style",
-    ),
-    ("py:class", "prompt_toolkit.shortcuts.prompt.CompleteStyle"): (
-        "py:class",
-        "prompt_toolkit.shortcuts.CompleteStyle",
-    ),
-    ("py:class", "prompt_toolkit.completion.base.Completer"): (
-        "py:class",
-        "prompt_toolkit.completion.Completer",
-    ),
-    ("py:class", "prompt_toolkit.lexers.base.Lexer"): (
-        "py:class",
-        "prompt_toolkit.lexers.Lexer",
-    ),
-}
-
-
-def add_intersphinx_aliases_to_inv(app):
-    from sphinx.ext.intersphinx import InventoryAdapter
-
-    inventories = InventoryAdapter(app.builder.env)
-
-    for alias, target in app.config.intersphinx_aliases.items():
-        alias_domain, alias_name = alias
-        target_domain, target_name = target
-
-        try:
-            found = inventories.main_inventory[target_domain][target_name]
-            inventories.main_inventory[alias_domain][alias_name] = found
-        except KeyError as e:
-            traceback.print_exc()
-            continue
-
-
-def setup(app):
-    app.add_config_value("intersphinx_aliases", {}, "env")
-    app.connect("builder-inited", add_intersphinx_aliases_to_inv)
