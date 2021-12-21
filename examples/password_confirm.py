85e0d2823d9c85618cb24b445c18cde20565d0b7
Kian Cross <kian@kiancross.co.uk>
2021-12-21 01:56:44 +0000

    Added password confirmation example

---
+++ examples/password_confirm.py
@@ -0,0 +1,18 @@
+import questionary
+
+
+def main():
+    while True:
+        x = questionary.password("Password").ask()
+        y = questionary.password("Repeat password").ask()
+
+    if x == y:
+        print(x)
+        break
+
+    else:
+        print("Passwords do not match. Try again.")
+
+
+if __name__ == "__main__":
+    main()
