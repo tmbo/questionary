d327fb3e477e36a37a45ddf036e548acce45bbc5
NicDom <83905171+NicDom@users.noreply.github.com>
2021-12-22 17:50:28 +0100

    Update questionary/prompts/date.py
    Fix typo
    
    Co-authored-by: Kian Cross <kian@kiancross.co.uk>

--- questionary/prompts/date.py
+++ questionary/prompts/date.py
@@ -279,7 +279,7 @@ class SimpleDateValidator(Validator):
 
 
 ###################################
-#  PARSING COMLETION AND VALIDATION
+#  PARSING COMPLETION AND VALIDATION
 ###################################
 
 
