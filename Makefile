tmbo/questionary

commit 6cdc221c1e0e2e79b785fd3b6a2d6618367e1fe3

Deprecate Python 3.6 & 3.7; get CI working again (#270)
* Dropped Python 3.6 and Python 3.7 support.

* Added Python 3.10 and Python 3.11 to all the relevant places.

* Updated pre-commit hooks to latest versions.

* Replaced deprecated `isort` flag `-sl` with current `--force-single-line-imports`.

* Change `extras` dependencies to modern Poetry dependency groups.
    * Removed all Sphinx-related doc dependencies from the main dependency group.

* Fixed a few minor typing issues with `merge_styles`.

* Explicitly set the `prompt_toolkit` version to be `<=3.0.36` because `3.0.37` has introduced some event-loop-related bug.

* `make clean` will now also clean the docs.

Makefile
@@ -30,6 +30,7 @@ clean:
 	rm -rf .mypy_cache/
 	rm -rf .pytest_cache/
 	rm -rf dist/
+	poetry run make -C docs clean
 
 install:
 	poetry install --extras "docs"
@@ -44,6 +45,7 @@ test:
 	poetry run pytest --cov questionary -v
 
 types:
+	poetry run mypy --version
 	poetry run mypy questionary
 
 docs:

docs/Makefile
@@ -3,7 +3,7 @@
 
 # You can set these variables from the command line.
 SPHINXOPTS    = -W --keep-going -n
-SPHINXBUILD   = sphinx-build
+SPHINXBUILD   = python -m sphinx
 SOURCEDIR     = .
 BUILDDIR      = build
 

.github/workflows/continuous-integration.yml
@@ -3,9 +3,9 @@ name: Continuous Integration
 on:
   push:
     branches:
-    - master
+      - master
     tags:
-    - '*'
+      - "*"
   pull_request:
 
 # SECRETS
@@ -18,35 +18,35 @@ jobs:
     runs-on: ubuntu-latest
 
     steps:
-    - name: Checkout git repository 🕝
-      uses: actions/checkout@v3
+      - name: Checkout git repository 🕝
+        uses: actions/checkout@v3
 
-    - name: Set up Python 3.7 🐍
-      uses: actions/setup-python@v4
-      with:
-        python-version: 3.7
+      - name: Set up Python 3.11 🐍
+        uses: actions/setup-python@v4
+        with:
+          python-version: "3.11"
 
-    - name: Install poetry 🦄
-      uses: Gr1N/setup-poetry@v7
+      - name: Install poetry 🦄
+        uses: Gr1N/setup-poetry@v8
 
-    - name: Load Poetry Cached Libraries ⬇
-      uses: actions/cache@v3
-      with:
-        path: ~/.cache/pypoetry
-        key: ${{ runner.os }}-poetry-${{ hashFiles('**/poetry.lock') }}
-        restore-keys: ${{ runner.os }}-poetry-
+      - name: Load Poetry Cached Libraries ⬇
+        uses: actions/cache@v3
+        with:
+          path: ~/.cache/pypoetry
+          key: ${{ runner.os }}-poetry-${{ hashFiles('**/poetry.lock') }}
+          restore-keys: ${{ runner.os }}-poetry-
 
-    - name: Install dependencies 🖥
-      run: poetry install --no-interaction
+      - name: Install dependencies 🖥
+        run: poetry install --no-interaction
 
-    - name: Lint Code 🎎
-      run: make lint
+      - name: Lint Code 🎎
+        run: make lint
 
-    - name: Check Types 📚
-      run: make types
+      - name: Check Types 📚
+        run: make types
 
-    - name: Check Version Numbers 🕸
-      run: poetry run python scripts/validate_version.py
+      - name: Check Version Numbers 🕸
+        run: poetry run python scripts/validate_version.py
 
   test:
     name: Run Tests
@@ -55,91 +55,88 @@ jobs:
     strategy:
       matrix:
         os: [ubuntu-latest, windows-latest, macos-latest]
-        python-version: [3.6, 3.7, 3.8, 3.9, '3.10']
+        python-version: ["3.8", "3.9", "3.10", "3.11"]
         promttoolkit: [3.*, 2.*]
         include:
           - promttoolkit: 3.0.29
             os: ubuntu-latest
-            python-version: '3.10'
           - promttoolkit: 3.0.19
             os: ubuntu-latest
-            python-version: '3.10'
 
     steps:
-    - name: Checkout git repository 🕝
-      uses: actions/checkout@v3
+      - name: Checkout git repository 🕝
+        uses: actions/checkout@v3
 
-    - name: Set up Python ${{ matrix.python-version }} 🐍
-      uses: actions/setup-python@v4
-      with:
-        python-version: ${{ matrix.python-version }}
+      - name: Set up Python ${{ matrix.python-version }} 🐍
+        uses: actions/setup-python@v4
+        with:
+          python-version: ${{ matrix.python-version }}
 
-    - name: Install poetry 🦄
-      uses: Gr1N/setup-poetry@v7
+      - name: Install poetry 🦄
+        uses: Gr1N/setup-poetry@v8
 
-    - name: Load Poetry Cached Libraries ⬇
-      uses: actions/cache@v3
-      with:
-        path: ~/.cache/pypoetry
-        key: ${{ runner.os }}-poetry-${{ hashFiles('**/poetry.lock') }}
-        restore-keys: ${{ runner.os }}-poetry-
+      - name: Load Poetry Cached Libraries ⬇
+        uses: actions/cache@v3
+        with:
+          path: ~/.cache/pypoetry
+          key: ${{ runner.os }}-poetry-${{ hashFiles('**/poetry.lock') }}
+          restore-keys: ${{ runner.os }}-poetry-
 
-    - name: Install dependencies 🖥
-      run: |
-        poetry install --no-interaction
-        poetry run pip install prompt_toolkit==${{ matrix.promttoolkit }}
+      - name: Install dependencies 🖥
+        run: |
+          poetry install --no-interaction
+          poetry run pip install prompt_toolkit==${{ matrix.promttoolkit }}
 
-    - name: Test Code 🔍
-      run: make test
+      - name: Test Code 🔍
+        run: make test
 
-    - name: Send Coverage Report 📊
-      env:
-        COVERALLS_REPO_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        COVERALLS_SERVICE_NAME: github
-        COVERALLS_PARALLEL: true
-      run: poetry run coveralls
-
+      - name: Send Coverage Report 📊
+        env:
+          COVERALLS_REPO_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          COVERALLS_SERVICE_NAME: github
+          COVERALLS_PARALLEL: true
+        run: poetry run coveralls
   post-test:
     name: Signal test completion
     needs: test
     runs-on: ubuntu-latest
     container: python:3-slim
     steps:
-    - name: Finished
-      run: |
-        pip3 install --upgrade coveralls
-        coveralls --service=github --finish
-      env:
-        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+      - name: Finished
+        run: |
+          pip3 install --upgrade coveralls
+          coveralls --service=github --finish
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 
   docs:
     name: Test Docs
     runs-on: ubuntu-latest
 
     steps:
-    - name: Checkout git repository 🕝
-      uses: actions/checkout@v3
+      - name: Checkout git repository 🕝
+        uses: actions/checkout@v3
 
-    - name: Set up Python 3.7 🐍
-      uses: actions/setup-python@v4
-      with:
-        python-version: 3.7
+      - name: Set up Python 3.11 🐍
+        uses: actions/setup-python@v4
+        with:
+          python-version: "3.11"
 
-    - name: Install poetry 🦄
-      uses: Gr1N/setup-poetry@v7
+      - name: Install poetry 🦄
+        uses: Gr1N/setup-poetry@v8
 
-    - name: Load Poetry Cached Libraries ⬇
-      uses: actions/cache@v3
-      with:
-        path: ~/.cache/pypoetry
-        key: ${{ runner.os }}-poetry-${{ hashFiles('**/poetry.lock') }}
-        restore-keys: ${{ runner.os }}-poetry
+      - name: Load Poetry Cached Libraries ⬇
+        uses: actions/cache@v3
+        with:
+          path: ~/.cache/pypoetry
+          key: ${{ runner.os }}-poetry-${{ hashFiles('**/poetry.lock') }}
+          restore-keys: ${{ runner.os }}-poetry
 
-    - name: Install dependencies 🖥
-      run: poetry install --no-interaction --extras "docs"
+      - name: Install dependencies 🖥
+        run: poetry install --no-interaction --with=docs
 
-    - name: Build docs ⚒️
-      run: make docs
+      - name: Build docs ⚒️
+        run: make docs
 
   deploy:
     name: Deploy to PyPI
@@ -147,21 +144,21 @@ jobs:
 
     # deploy will only be run when there is a tag available
     if: github.event_name == 'push' && startsWith(github.ref, 'refs/tags')
-    needs: [quality, test, docs]  # only run after all other stages succeeded
+    needs: [quality, test, docs] # only run after all other stages succeeded
 
     steps:
-    - name: Checkout git repository 🕝
-      uses: actions/checkout@v3
+      - name: Checkout git repository 🕝
+        uses: actions/checkout@v3
 
-    - name: Set up Python 3.7 🐍
-      uses: actions/setup-python@v4
-      with:
-        python-version: 3.7
+      - name: Set up Python 3.11 🐍
+        uses: actions/setup-python@v4
+        with:
+          python-version: "3.11"
 
-    - name: Install poetry 🦄
-      uses: Gr1N/setup-poetry@v7
+      - name: Install poetry 🦄
+        uses: Gr1N/setup-poetry@v8
 
-    - name: Build ⚒️ Distributions
-      run: |
-        poetry build
-        poetry publish -u __token__ -p ${{ secrets.PYPI_TOKEN }}
+      - name: Build ⚒️ Distributions
+        run: |
+          poetry build
+          poetry publish -u __token__ -p ${{ secrets.PYPI_TOKEN }}

.pre-commit-config.yaml
@@ -3,23 +3,30 @@ repos:
     rev: v1.1
     hooks:
       - id: autoflake
-        args: ['-i', '--remove-all-unused-imports']
+        args: ["-i", "--remove-all-unused-imports"]
 
   - repo: https://github.com/psf/black
-    rev: 22.3.0
+    rev: 23.3.0
     hooks:
       - id: black
-        args: ['-l', '88']
+        args:
+          - "--target-version=py38"
+          - "--target-version=py39"
+          - "--target-version=py310"
+          - "--target-version=py311"
+          - "--line-length=88"
 
   - repo: https://github.com/pycqa/isort
-    rev: 5.10.1
+    rev: 5.12.0
     hooks:
       - id: isort
         name: isort (python)
-        args: ['-sl', '--profile', 'black']
+        args:
+          - "--force-single-line-imports"
+          - "--profile=black"
 
   - repo: https://github.com/pycqa/flake8
-    rev: 3.9.2
+    rev: 6.0.0
     hooks:
       - id: flake8
-        args: ['--max-line-length', '120', '--max-doc-length', '140']
+        args: ["--max-line-length", "120", "--max-doc-length", "140"]

docs/conf.py
@@ -33,7 +33,7 @@ html_theme_options = {
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "prompt_toolkit": ("https://python-prompt-toolkit.readthedocs.io/en/master/", None),
+    "prompt_toolkit": ("https://python-prompt-toolkit.readthedocs.io/en/3.0.36/", None),
 }
 
 autodoc_member_order = "alphabetical"

pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "poetry.core.masonry.api"
 
 [tool.black]
 line-length = 88
-target-version = [ "py36", "py37", "py38",]
+target-version = ["py38", "py39", "py310", "py311"]
 exclude = "((.eggs | .git | .pytype | .pytest_cache | build | dist))"
 
 [tool.poetry]
@@ -21,10 +21,10 @@ classifiers=[
     "License :: OSI Approved :: MIT License",
     # supported python versions
     "Programming Language :: Python",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Topic :: Software Development :: Libraries",
 ]
 keywords = [ "cli", "ui", "inquirer", "questions", "prompt",]
@@ -32,21 +32,23 @@ readme = "README.md"
 license = "MIT"
 
 [tool.poetry.dependencies]
-python = "^3.6"
-prompt_toolkit = ">=2.0,<4.0"
-Sphinx = {version = ">=4.1,<6.0", optional = true}
-sphinx-rtd-theme = {version = ">=0.5,<1.1", optional = true}
-sphinx-autobuild = {version = ">=2020.9.1,<2022.0.0", optional = true}
-sphinx-copybutton = {version = ">=0.3.1,<0.6.0", optional = true}
-sphinx-autodoc-typehints = {version = "^1.11.1", optional = true}
+python = "^3.8"
+prompt_toolkit = ">=2.0,<=3.0.36"  # once https://github.com/prompt-toolkit/python-prompt-toolkit/issues/1726 is fixed, this can be changed to ">=2.0,<4.0"
 
-[tool.poetry.extras]
-docs = ["Sphinx", "sphinx-rtd-theme", "sphinx-autobuild", "sphinx-copybutton", "sphinx-autodoc-typehints"]
+[tool.poetry.group.docs]
+optional = true
 
-[tool.poetry.dev-dependencies]
+[tool.poetry.group.docs.dependencies]
+Sphinx = ">=4.1,<6.0"
+sphinx-rtd-theme = ">=0.5,<1.1"
+sphinx-autobuild = ">=2020.9.1,<2022.0.0"
+sphinx-copybutton = ">=0.3.1,<0.6.0"
+sphinx-autodoc-typehints = "^1.11.1"
+
+[tool.poetry.group.dev.dependencies]
 pytest-cov = "^3.0.0"
 pytest = "^7.0.1"
 coveralls = "^3.3.1"
-mypy = "^0.971"
-toml = "^0.10.1"
+mypy = "^1.2.0"
+toml = "^0.10.2"
 pre-commit = { version = "^2.20.0", python = ">=3.7,<4.0"}

questionary/prompts/autocomplete.py
@@ -16,12 +16,11 @@ from prompt_toolkit.lexers import SimpleLexer
 from prompt_toolkit.shortcuts.prompt import CompleteStyle
 from prompt_toolkit.shortcuts.prompt import PromptSession
 from prompt_toolkit.styles import Style
-from prompt_toolkit.styles import merge_styles
 
 from questionary.constants import DEFAULT_QUESTION_PREFIX
-from questionary.constants import DEFAULT_STYLE
 from questionary.prompts.common import build_validator
 from questionary.question import Question
+from questionary.styles import merge_styles_default
 
 
 class WordCompleter(Completer):
@@ -176,8 +175,7 @@ def autocomplete(
     Returns:
         :class:`Question`: Question instance, ready to be prompted (using ``.ask()``).
     """
-
-    merged_style = merge_styles([DEFAULT_STYLE, style])
+    merged_style = merge_styles_default([style])
 
     def get_prompt_tokens() -> List[Tuple[str, str]]:
         return [("class:qmark", qmark), ("class:question", " {} ".format(message))]
@@ -202,7 +200,7 @@ def autocomplete(
             match_middle=match_middle,
         )
 
-    p = PromptSession(
+    p: PromptSession = PromptSession(
         get_prompt_tokens,
         lexer=SimpleLexer("class:answer"),
         style=merged_style,

questionary/prompts/checkbox.py
@@ -12,18 +12,17 @@ from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.keys import Keys
 from prompt_toolkit.styles import Style
-from prompt_toolkit.styles import merge_styles
 
 from questionary import utils
 from questionary.constants import DEFAULT_QUESTION_PREFIX
 from questionary.constants import DEFAULT_SELECTED_POINTER
-from questionary.constants import DEFAULT_STYLE
 from questionary.constants import INVALID_INPUT
 from questionary.prompts import common
 from questionary.prompts.common import Choice
 from questionary.prompts.common import InquirerControl
 from questionary.prompts.common import Separator
 from questionary.question import Question
+from questionary.styles import merge_styles_default
 
 
 def checkbox(
@@ -115,9 +114,8 @@ def checkbox(
             "Emacs keys."
         )
 
-    merged_style = merge_styles(
+    merged_style = merge_styles_default(
         [
-            DEFAULT_STYLE,
             # Disable the default inverted colours bottom-toolbar behaviour (for
             # the error message). However it can be re-enabled with a custom
             # style.
@@ -179,7 +177,6 @@ def checkbox(
         return [c.value for c in ic.get_selected_values()]
 
     def perform_validation(selected_values: List[str]) -> bool:
-
         verdict = validate(selected_values)
         valid = verdict is True
 
@@ -192,7 +189,7 @@ def checkbox(
             error_message = FormattedText([("class:validation-toolbar", error_text)])
 
         ic.error_message = (
-            error_message if not valid and ic.submission_attempted else None
+            error_message if not valid and ic.submission_attempted else None  # type: ignore[assignment]
         )
 
         return valid
@@ -270,7 +267,6 @@ def checkbox(
 
     @bindings.add(Keys.ControlM, eager=True)
     def set_answer(event):
-
         selected_values = get_selected_values()
         ic.submission_attempted = True
 

commit 6e98c4964480d6b848c41e7b96905d79bba80ecf

Fix bug in Makefile introduced in #270

Makefile
@@ -33,7 +33,7 @@ clean:
 	poetry run make -C docs clean
 
 install:
-	poetry install --extras "docs"
+	poetry install --with="docs"
 
 develop: install
 	poetry run pre-commit install